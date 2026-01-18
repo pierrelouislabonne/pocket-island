2adbe87275256d58c7060851329fcd8efe22cc0b
Pierre-Louis <pierrelouis.labonne@gmail.com>
2026-01-18 14:01:22 +0100

    Updating _init

--- _init.js
+++ _init.js
@@ -4,5 +4,7 @@
 
 // UTILITIES
 slater_import('/project/18238/page/53445.js'); // CURSOR
-slater_import('/project/18238/page/53526.js'); // CANVAS ROTATION
-//
+slater_import('/project/18238/page/53943.js'); // CONTROLS (PITCHS)
+slater_import('/project/18238/page/53526.js'); // SCENE ROTATION
+slater_import('/project/18238/page/53944.js'); // DAY/NIGHT
+slater_import('/project/18238/page/53945.js'); // RESPONSIVE
