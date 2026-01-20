90e4c0fc7d77c3bf723ff10abc01f986985c6968
Pierre-Louis <pierrelouis.labonne@gmail.com>
2026-01-20 14:32:19 +0100

    Updating _init

--- _init.js
+++ _init.js
@@ -14,6 +14,9 @@
  * ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝      
  */
 
+/* Code for https://pocket-island.webflow.io/ */
+/* Designed by Pierre-Louis Design https://pierrelouis.design */
+
 /* ---------------------------------------------------------------------------- */
 /*                                    INIT                                      */
 /* ---------------------------------------------------------------------------- */
@@ -25,6 +28,7 @@ slater_import('/project/18238/page/53943.js'); // CONTROLS (PITCHS)
 slater_import('/project/18238/page/53526.js'); // ISLAND CYCLE
 slater_import('/project/18238/page/53944.js'); // SKY CYCLE
 slater_import('/project/18238/page/53945.js'); // BREAKPOINT WATCHER
+slater_import('/project/18238/page/54044.js'); // FOOTER DATE
 
 // --- CLEANUP ---
 
