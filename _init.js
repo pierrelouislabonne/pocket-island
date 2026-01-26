4b7844639660b15a8b8aa8d7976743ac12c51d57
Pierre-Louis <pierrelouis.labonne@gmail.com>
2026-01-26 12:05:05 +0100

    Updating _init

--- _init.js
+++ _init.js
@@ -27,6 +27,7 @@ slater_import('/project/18238/page/53445.js'); // CUSTOM CURSOR
 slater_import('/project/18238/page/53943.js'); // CONTROLS (PITCHS)
 slater_import('/project/18238/page/53526.js'); // ISLAND CYCLE
 slater_import('/project/18238/page/53944.js'); // SKY CYCLE
+slater_import('/project/18238/page/54342.js'); // VOLCANO
 slater_import('/project/18238/page/53945.js'); // BREAKPOINT WATCHER
 slater_import('/project/18238/page/54044.js'); // FOOTER DATE
 
